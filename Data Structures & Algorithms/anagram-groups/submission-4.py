4772431a9c74230c1aadf92efd20eeab8a3c56e2
Sebastien <127785563+SanAdams@users.noreply.github.com>
2026-03-30 15:49:48 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        frequency_arrays = {}
+
+       # Turn each word into a frequency array
+        for string in strs:
+            frequency = [0] * 26
+            for i in range(len(string)):
+                frequency[ord(string[i]) - 97] += 1
+            frequency_arrays[string] = tuple(frequency)
+
+        # Map the frequency arrays(words) to their sublists
+        sublists = {}
+        for i in range(len(strs)):
+            current_string = strs[i]
+            if frequency_arrays[current_string] in sublists:
+                sublists[frequency_arrays[current_string]].append(current_string)
+            else:
+                sublists[frequency_arrays[current_string]] = []
+                sublists[frequency_arrays[current_string]].append(current_string)
+
+        return list(sublists.values())
